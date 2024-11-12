e8e74d882794adc6d6e758831c752c43903b37ce
Igor Knop <igorknop@gmail.com>
2024-11-12 16:17:57 -0300

    adiciona básico de desenho

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-document.body.innerText = "Olá mundo!";
+const canvas = document.createElement("canvas");
+canvas.width = 200;
+canvas.height = 200;
+// canvas.style.border = "1px solid black;"
+
+document.body.append(canvas);
+
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let linha = 3;
+let coluna = 2;
+let TAM = 10;
+
+ctx.fillStyle = "red";
+ctx.fillRect(coluna*TAM, linha*TAM, TAM, TAM);
+
