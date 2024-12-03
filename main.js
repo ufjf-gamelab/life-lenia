baa3d972c652a3381068ded2ee31c3ce84a013fa
Rayane Oliveira <rayanecontato1@gmail.com>
2024-12-03 16:55:08 -0300

    Corrige indices

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let l = 0; l < 20; l++) {
   A[l] = [];
   B[l] = [];
   for (let c = 0; c < 20; c++) {
-    if (Math.random() < 0.3) {
+    if (Math.random() < -0.3) {
       A[l][c] = 1;
     } else {
       A[l][c] = 0;
@@ -25,12 +25,25 @@ for (let l = 0; l < 20; l++) {
   }
 }
 
+A[0][10] = 1;
+A[1][10] = 1;
+A[2][10] = 1;
+A[2][11] = 1;
+A[1][12] = 1;
+
+A[10][0] = 1;
+A[10][1] = 1;
+A[10][2] = 1;
+A[9][2] = 1;
+A[8][1] = 1;
+
+
 let vez = true;
 let dt = 0;
 let t0 = 0;
 let cooldown = 0;
 function passo(t) {
-  dt = (t - t0)/1000;
+  dt = (t - t0) / 1000;
 
   cooldown += dt;
   if (cooldown > 0.1) {
@@ -60,17 +73,21 @@ A dead cell will be brought back to live if it has exactly three live neighbors.
  */
 
 function atualizaMatriz(D, O) {
-  for (let l = 0 + 1; l < 20 - 1; l++) {
-    for (let c = 0 + 1; c < 20 - 1; c++) {
-      const v = contaVizinhos(O, l, c);
+  for (let l = 0; l < 20; l++) {
+    //linha
+    for (let c = 0; c < 20; c++) {
+      //coluna
+      const v = contaVizinhos(O, l, c); //conta os vizinhos posição
       if (O[l][c] === 1) {
         if (v < 2 || v > 3) {
+          //mais que 3 ou menos que 2 morre
           D[l][c] = 0;
         } else {
           D[l][c] = 1;
         }
       } else {
         if (v === 3) {
+          // 3 vizionhos revive
           D[l][c] = 1;
         } else {
           D[l][c] = 0;
@@ -85,19 +102,40 @@ function limpaTela() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+//
 function contaVizinhos(M, l, c) {
+  
   let total = 0;
+  const numLinhas = M.length;
+  const numColunas = M[0].length;
 
-  total += M[l - 1][c];
-  total += M[l - 1][c - 1];
-  total += M[l - 1][c + 1];
-  total += M[l + 1][c];
-  total += M[l + 1][c - 1];
-  total += M[l + 1][c + 1];
-  total += M[l][c - 1];
-  total += M[l][c + 1];
+  // Obter os vizinhos, considerando bordas opostas
+  total += v(M, l - 1, c) // Cima
+  total += M[getIndice(l - 1, numLinhas)][getIndice(c - 1, numColunas)]; // Diagonal superior esquerda
+  total += M[getIndice(l - 1, numLinhas)][getIndice(c + 1, numColunas)]; // Diagonal superior direita
+  total += M[getIndice(l + 1, numLinhas)][c]; // Baixo
+  total += M[getIndice(l + 1, numLinhas)][getIndice(c - 1, numColunas)]; // Diagonal inferior esquerda
+  total += M[getIndice(l + 1, numLinhas)][getIndice(c + 1, numColunas)]; // Diagonal inferior direita
+  total += M[l][getIndice(c - 1, numColunas)]; // Esquerda
+  total += M[l][getIndice(c + 1, numColunas)]; // Direita
 
   return total;
+}
+
+// Calcula o índice, considerando bordas opostas
+function getIndice(indice, maxTam) {
+  if (indice > 0) {
+    return (indice % maxTam);
+  } else {
+    return (-Math.abs(indice % maxTam) + maxTam) % maxTam;
+  }
+}
+
+function v(M, l, c)
+{
+  const TL = M.length;
+  const TC = M[0].length; 
+  return M[getIndice(l, TL)][getIndice(c, TC)];
 }
 
 function desenhaMatriz(M) {
