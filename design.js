2bbd890c1bc013a89cc862c550e9b76e05034ca5
Rayane Oliveira <rayanecontato1@gmail.com>
2025-01-21 08:28:18 -0300

    Acrescenta pintaAzuis no arquivo de design

--- design.js
+++ design.js
@@ -14,17 +14,31 @@ export function desenhaMatriz(M, ctx, TAM) {
     }
   }
 
-  export function desenhaQuadrado(linha, coluna, ctx, TAM) {
+export function desenhaQuadrado(linha, coluna, ctx, TAM) {
     ctx.fillStyle = "red";
     ctx.fillRect(linha * TAM, coluna * TAM, TAM, TAM);
   }
   
-  export function desenhaQuadradoAzul(linha, coluna, ctx, TAM) {
+export function desenhaQuadradoAzul(linha, coluna, ctx, TAM) {
     ctx.fillStyle = "blue";
     ctx.fillRect(linha * TAM, coluna * TAM, TAM, TAM);
   }
 
-  export function limpaTela(ctx, canvas) {
+export function limpaTela(ctx, canvas) {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+export function pintaAzuis(M, lc, cc, r, ctx, TAM) {
+    const TL = M.length;
+    const TC = M[0].length; 
+    
+    for(let l = lc - r; l <= lc + r; l++)
+    {
+      for(let c = cc - r; c <= cc + r; c++)
+      {
+          if(l === lc && c === cc) continue;
+          design.desenhaQuadradoAzul(getIndice(l, TL), getIndice(c, TC), ctx, TAM);
+      }
+    }
   }
