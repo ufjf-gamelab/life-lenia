8da8f997fb0325b78197ceea4b692b53479bec63
Rayane Oliveira <rayanecontato1@gmail.com>
2025-05-12 01:28:29 -0300

    função growth que analisa estado das células com base nas regras do jogo

--- convolution.js
+++ convolution.js
@@ -31,3 +31,23 @@ export function getIndice(indice, maxTam) {
     return (-Math.abs(indice % maxTam) + maxTam) % maxTam;
   }
 }
+
+function growth(U) {  //chamei U para analizar os vizinhos e aplicar as regras 
+  const TL = U.length;
+  const TC = U[0].length;
+  let G = Array.from({ length: TL }, () => Array(TC).fill(0));
+
+  for (let i = 0; i < TL; i++) {
+    for (let j = 0; j < TC; j++) {
+      if (U[i][j] === 3) {  //Se tem 3 vizinhos, nasce
+        G[i][j] = 1;
+      } else if (U[i][j] < 2 || U[i][j] > 3) {   //Se tem pouco ou muito, morre
+        G[i][j] = -1;
+      } else {   //se tem 2 vizinhos mantém o estado atual
+        G[i][j] = 0;
+      }
+    }
+  }
+  return G;  //retorno matriz que aplica as regras do game
+}
+
