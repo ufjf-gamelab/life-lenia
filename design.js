ca24da223c592d9490e635ab148cf116bb8d5f8e
Rayane Oliveira <rayanecontato1@gmail.com>
2025-06-12 14:38:24 -0300

    apaga comentarios e coisas desnecessárias

--- design.js
+++ design.js
@@ -3,25 +3,20 @@ import { getIndice } from './primordia.js';
 export function desenhaMatriz(M, ctx, TAM) {
     const TL = M.length;
     const TC = M[0].length;
+    
     const states = 12; //12 estados do primordia
     for (let l = 0; l < TL; l++) {
       for (let c = 0; c < TC; c++) {
-        //  if (M[l][c] === 1) {
-        //   desenhaQuadrado(l, c, ctx, TAM);
-        // }
-        // if (M[l][c] === 2) {
-        //   desenhaQuadradoAzul(l, c, ctx, TAM); // Azul
-        // }
          if (M[l][c] > 0 && M[l][c] <= states) {  //verifica se está entre 1 e 12 (0 não desenha)
-          // console.log(`Desenhando célula em [${l}, ${c}] com valor ${M[l][c]}`);
           const intensidade = M[l][c] / states;  //ex: 6/12 tem 50% da intensidade
           ctx.fillStyle = `rgb(${Math.floor(intensidade * 255)}, 0, 0)`; //variação cor: quanto maior o valor, mais forte fica o vermelho
           desenhaQuadrado(l, c, ctx, TAM);
           escreveTexto(l, c, ctx, TAM, M[l][c]);
+          
         }
       }
     }
-    debugger;
+    
   }
 
 export function desenhaQuadrado(linha, coluna, ctx, TAM) {
