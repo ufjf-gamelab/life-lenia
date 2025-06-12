7e4ea41f5cfc5850f3839ead3a0a6582a3daed14
Rayane Oliveira <rayanecontato1@gmail.com>
2025-06-12 14:40:17 -0300

    kernel normalizado na main com erros corrigidos

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { atualizaMatriz, stampDiagonalLarge, stampOscilatingLarge } from './prim
 
 const canvas = document.createElement("canvas");
 const TAM = 15;
-const DIM = 100;
+const DIM = 20;
 canvas.width = TAM*DIM;
 canvas.height = TAM*DIM;
 
@@ -28,14 +28,45 @@ for (let l = 0; l < DIM; l++) {
 }
 
 
-stampOscilatingLarge(A, 15, 15);
-stampDiagonalLarge(A, 50, 50);
-// startGlider(A);
 
 
 let vez = true;
 let dt = 0;
 let t0 = 0;
+
+let options = {
+  states:12,
+  K:20,
+  L:4,
+  M:1,
+  N:2,
+  kernel: [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ],
+}
+
+//   options.K_sum = options.states * options.kernel.flat().reduce((a,c)=>a+c, 0);  //estava calculando a soma do kernel mas depois multiplicava por states gerando um valor muito maior
+//   options.kernelNorm = options.kernel.map((l)=>l.map((i)=>i/options.K_sum));
+//   options.KNorm = options.K/options.K_sum;  //20/96
+//   options.LNorm = options.L/options.K_sum;
+//   options.MNorm = options.M/options.K_sum;
+//   options.NNorm = options.N/options.K_sum;
+
+  const somaKernel = options.kernel.flat().reduce((a, c)=>a+c, 0);  //não multipliquei por states
+  options.kernelNorm = options.kernel.map((l) =>l.map((i)=>i/somaKernel));
+  options.KNorm = options.K/somaKernel;
+  options.LNorm = options.L/somaKernel;
+  options.MNorm = options.M/somaKernel;
+  options.NNorm = options.N/somaKernel;
+
+
+//stampOscilatingLarge(A, 10, 10);
+stampDiagonalLarge(A, 10, 10);
+
+// startGlider(A);
+
 
 function passo(t) {
   dt = (t - t0) / 1000;
@@ -44,10 +75,10 @@ function passo(t) {
     limpaTela(ctx, canvas);
     // pintaAzuis( A, 1, 1, 3);
     if (vez) {
-      atualizaMatriz(B, A);
+      atualizaMatriz(B, A, options);
       desenhaMatriz(B, ctx, TAM);
     } else {
-      atualizaMatriz(A, B);
+      atualizaMatriz(A, B, options);
       desenhaMatriz(A, ctx, TAM);
     }
     vez = !vez;
